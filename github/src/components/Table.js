c611d731062e8a5084e80e1f04833fbd056005ef
nvchaitanya <chathan2569@gmail.com>
2020-04-27 23:36:30 +0530

    updated

--- github/src/components/Table.js
+++ github/src/components/Table.js
@@ -1,10 +1,34 @@
 import React from 'react'
 class Table extends React.Component{
     render(){
-        console.log(this.props.tabdata)
         return(
             <div>
-
+                <div className="col-11 offset-1 col-md-12 offset-md-0 col-lg-12 table-responsive">
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th>Profile Pic</th>
+                                <th>Login Name</th>
+                                <th>Id</th>
+                                <th>HTML url</th>
+                                <th>Repo's Link</th>
+                                <th>Profile Info</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.tabdata.map(element=>
+                                <tr key={element.id}>
+                                    <td><img src={element.avatar_url} alt="pic" height='100px'/></td>
+                                    <td className="pt-5">{element.login}</td>
+                                    <td className="pt-5">{element.id}</td>
+                                    <td className="pt-5"><a href={element.html_url} target="_blank">External Github</a></td>
+                                    <td className="pt-5"><a href={element.repos_url} target="_blank">Repos</a></td>
+                                    <td className="pt-5"><button className="btn btn-primary">Profile Information</button></td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
--- github/src/components/User.js
+++ github/src/components/User.js
@@ -10,7 +10,7 @@ class User extends React.Component{
             language:'',
             userData:[],
             isdata:false,
-            isrepo:false
+            isrepo:false,
         }
     }
 
@@ -19,28 +19,64 @@ class User extends React.Component{
             [e.target.name]:e.target.value
         })
     }
-    
+
+    handleSelect=(e)=>{
+        (e.target.value==='repo')?this.setState({isrepo:true}):this.setState({isrepo:false})
+    }
+
     handleSubmit=(e)=>{
         e.preventDefault()
-        const {name}=this.state
-        const requestParams={
-            method:'get',
-            url:'https://api.github.com/search/users',
-            params:{
-                q:name
+        if(!this.state.isrepo){
+            const {name}=this.state
+            const requestParams={
+                method:'get',
+                url:'https://api.github.com/search/users',
+                params:{
+                    q:name
+                }
             }
+            axios(requestParams)
+            .then(res=>{
+                if(res){
+                    const data = res.data.items
+                    this.setState({
+                        userData:[...data],
+                        isdata:true
+                    })
+                }
+            })
+            .catch(error=>console.log(error))
         }
-        axios(requestParams)
-        .then(res=>{
-            if(res){
-                const data = res.data.items
-                this.setState({
-                    userData:[...data],
-                    isdata:true
-                })
+        else{
+            const {name,language} = this.state
+            const requestParams = {
+                method:'get',
+                url:'https://api.github.com/search/repositories',
+                params:{
+                    q:name,
+                    language:language
+                }
             }
+            axios(requestParams)
+            .then(res=>{
+                if(res){
+                    const data = res.data.items.map(element=>element.owner)
+                    this.setState({
+                        userData:[...data],
+                        isdata:true
+                    })
+                }
+            })
+        }
+        this.reset()
+    }
+
+    reset=()=>{
+        this.setState({
+            name:'',
+            language:'',
+            // isrepo:false,
         })
-        .catch(error=>console.log(error))
     }
     render(){
         return(
@@ -49,18 +85,20 @@ class User extends React.Component{
                 <div className="col-10 offset-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 my-3">
                     <form onSubmit={this.handleSubmit}>
                         <div className="form-group">
-                            <select className="form-control">
-                                <option value="--">Search By</option>
+                            <select className="form-control" onClick={this.handleSelect} required>
+                                <option value="">Search By</option>
                                 <option value="user">UserName</option>
                                 <option value="repo">Repository</option>
                             </select>
                         </div>
                         <div className="form-group">
-                            <input type="text" name="name" className="form-control" onChange={this.handleChange} placeholder="Search by username or Repo"/>
+                            <input type="text" name="name" value={this.state.name} className="form-control" onChange={this.handleChange} placeholder="Search by username or Repo" required/>
                         </div>
-                        <div className="form-group">
-                            <input type="text" name="language" className="form-control" onChange={this.handleChange} placeholder="Search by Language"/>
-                        </div>
+                        {this.state.isrepo && 
+                            <div className="form-group">
+                                <input type="text" name="language" value={this.state.language} className="form-control" onChange={this.handleChange} placeholder="Search by Language" required/>
+                            </div>
+                        }
                         <div className="text-center">
                             <button className="btn btn-primary">SUBMIT</button>
                         </div>
